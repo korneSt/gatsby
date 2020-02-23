0a01b62bc9603be833a7d3256d785d60ef01e0a7
korSt <k4z1kpl@gmail.com>
2020-02-23 13:08:15 +0100

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const About = () => (
-  <div>
-    <p>About page</p>
-  </div>
+  <Layout>
+    <SEO title="About page" />
+    <p>This is about page.</p>
+  </Layout>
 )
 
 export default About
